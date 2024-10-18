7b9c7b104db3945616da03f1b06fa6cc3b1364fd
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2024-10-18 16:04:28 -0500

    Able to POST todo tasks successfully to database

--- TaskTracker/Controllers/TasksController.cs
+++ TaskTracker/Controllers/TasksController.cs
@@ -1,13 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using System.Numerics;
+using TaskTracker.Data;
+using TaskTracker.Models;
 
 namespace TaskTracker.Controllers
 {
     public class TasksController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public TasksController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddTaskViewModel task)
+        {
+            var newTask = new Models.Entities.Task
+            {
+                Title = task.Title,
+                Description = task.Description,
+                CreatedDate = task.CreatedDate,
+                DueDate = task.DueDate,
+                Complete = task.Complete
+            };
+
+
+            await dbContext.Tasks.AddAsync(newTask);
+            await dbContext.SaveChangesAsync();
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+
         }
     }
 }
---
+++ TaskTracker/Models/AddTaskViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskTracker.Models
+{
+    public class AddTaskViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Complete { get; set; }
+
+    }
+}
