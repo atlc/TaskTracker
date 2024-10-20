2bd8f788328396d98145f513209c111c4ea2ca53
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2024-10-20 17:29:38 -0500

    CRUD operations mostly completed, finalizing delete and then polishing UI

--- TaskTracker/Controllers/TasksController.cs
+++ TaskTracker/Controllers/TasksController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualBasic;
-using System.Numerics;
+using Microsoft.EntityFrameworkCore;
 using TaskTracker.Data;
 using TaskTracker.Models;
 
@@ -37,13 +36,54 @@ namespace TaskTracker.Controllers
             await dbContext.Tasks.AddAsync(newTask);
             await dbContext.SaveChangesAsync();
 
-            return View();
+            return RedirectToAction("List", "Tasks");
         }
 
         [HttpGet]
         public async Task<IActionResult> List()
         {
+            var tasks = await dbContext.Tasks.ToListAsync();
+            return View(tasks);
+        }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid Id)
+        {
+            var task = await dbContext.Tasks.FindAsync(Id);
+            return View(task);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditTaskViewModel task)
+        {
+            var editableTask = await dbContext.Tasks.FindAsync(task.Id);
+
+            if (editableTask is not null)
+            {
+                editableTask.Title = task.Title;
+                editableTask.Description = task.Description;
+                editableTask.CreatedDate = task.CreatedDate;
+                editableTask.DueDate = task.DueDate;
+                editableTask.Complete = task.Complete;
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Tasks");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(EditTaskViewModel task)
+        {
+            var removeableTask = await dbContext.Tasks.FindAsync(task.Id);
+
+            if (removeableTask is not null)
+            {
+                dbContext.Tasks.Remove(removeableTask);
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToPage("List", "Tasks");
         }
     }
 }
---
+++ TaskTracker/Models/EditTaskViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskTracker.Models
+{
+    public class EditTaskViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Complete { get; set; }
+
+    }
+}
